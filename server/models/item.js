50379796f9fb4f57ad9240797235641ab198eb17
fenghsi  yu <fenghsi.yu@stonybrook.edu>
2019-10-15 16:09:38 -0400

    schema update

--- server/models/item.js
+++ server/models/item.js
@@ -1,34 +1,20 @@
-const express = require("express");
-const router = express.Router();
-const Items = require('../models/item');
-const uuidv4 = require('uuid/v4');
-//add item to db
-router.post('/additem', async function(req, res, next) {
-    //deal error cases??
-    if(req.body.content==null){
-        return res.json({
-            status: "error",
-            error: "Empty content is not allowed"
-        });
-    }
-    const id = uuidv4();
-    const newitem = new Items({
-        id: id,
-        username: req.body.username,
-        content: req.body.content,
-        childType: req.body.childType,
-        retweeted: 0,
-        property: { likes:0 },
-        timestamp: new Date()
-    });
-    await newitem.save();
-    const item = await Items.findOne({ id:id });
-    console.log(item);
-    return res.json({
-        status: "OK",
-        id : id
-    });
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const itemSchema = new Schema({
+    id: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    username: String,
+    content: String, 
+    childType: String,
+    retweeted: Number,
+    property:{
+        likes: Number,
+    },
+    timestamp: Date
 });
 
-
-module.exports = router;
+module.exports = mongoose.model('Items', itemSchema);
