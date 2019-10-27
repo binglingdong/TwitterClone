fa9c9a8cf83456f7d0361c9700b8503fc9038e26
mandykwokk <mandy25093@gmail.com>
2019-10-27 14:32:57 -0400

    follow and delete

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Home from './component/twitter/Home';
 import AddItem from './component/twitter/AddItem';
 import SearchResult from './component/search/SearchResult';
 import Search from'./component/search/Search';
+import Item from'./component/twitter/Item';
 import { notification } from 'antd';
 
 function App(props) {
@@ -140,7 +141,7 @@ function App(props) {
                 <Route exact path="/" render={() => (<Home/>)} />
                 <Route exact path = "/search" render={() => (<Search handleSearch= {handleSearch} handleGetTwitter={handleGetTwitter}/>)} />
                 <Route exact path="/searchresult" render={() => (<SearchResult searchResult={searchResult} />)} />
-                <Route path="/item/:id" render={() => (<SearchItem item={item} handleDeleteTwitter={handleDeleteTwitter}/>)} />
+                <Route path="/item/:id" render={() => (<Item item={item} handleDeleteTwitter={handleDeleteTwitter}/>)} />
                 {!user && 
                     <React.Fragment>
                         <Route path="/verify" render={() => (<Verify handleVerifcation={handleVerifcation}/>)} />
